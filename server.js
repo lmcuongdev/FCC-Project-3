2c8aed11fa8f84451280a1a3b4848eb603ef340a
lmcuongdev <cuong2000hy@gmail..com>
2020-06-12 14:52:39 +0700

    add user + log excercises

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const shortid = require("shortid");
 
 const cors = require("cors");
 
@@ -12,8 +13,32 @@ mongoose.connect(
 
 const db = mongoose.connection;
 
-User = mongoose.model("users", new mongoose.Schema({ username: String }));
-User.find({}, (err, res) => console.log(res));
+User = mongoose.model(
+  "users",
+  new mongoose.Schema(
+    {
+      username: { type: String, unique: true },
+      _id: {
+        type: String,
+        default: shortid.generate,
+      },
+      log: [
+        {
+          description: String,
+          duration: Number,
+          date: Date,
+          _id: { required: false },
+        },
+      ],
+    },
+    { versionKey: false }
+  )
+);
+
+User.find({}).then((res) => {
+  console.log(res);
+  console.log("===========================================");
+});
 
 app.use(cors());
 
@@ -23,6 +48,84 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+// my code
+app.post("/api/exercise/new-user", (req, res) => {
+  const { username } = req.body;
+  User.create({ username: username }, (err, user) => {
+    if (err) res.send("username exists");
+    else {
+      console.log(user);
+      res.json({ username: user.username, _id: user._id });
+      // res.json(result.map((v) => ({ _id: v._id, username: v.username })));
+    }
+  });
+});
+
+app.get("/api/exercise/users", (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) res.send(err);
+    else
+      res.json(
+        users.map((u) => ({
+          _id: u._id,
+          username: u.username,
+        }))
+      );
+  });
+});
+
+app.post("/api/exercise/add", (req, res) => {
+  const { userId, description } = req.body;
+  const date = req.body.date ? new Date(req.body.date) : new Date();
+  const duration = Number(req.body.duration);
+  if (date.toString() === "Invalid Date" || duration.toString() === "NaN")
+    return res.send("Invalid date or duration input");
+  // find that user
+  const idValidate = { _id: userId };
+  User.findOne(idValidate).then((user) => {
+    // if user exists
+    if (user) {
+      const info = { description, duration, date };
+      console.log(info);
+      // do the update
+      const updatedLog = [...user.log, info];
+      User.updateOne(idValidate, { log: updatedLog }).then((result) => {
+        console.log("Updated");
+        res.json({
+          userId,
+          ...info,
+          date: info.date.toDateString(),
+          username: user.username,
+        });
+      });
+    }
+    // canr find that userid
+    else {
+      res.send("userId not exist");
+    }
+  });
+});
+
+app.get("/api/exercise/log", (req, res) => {
+  console.log(req.query);
+  const { userId, from, to, limit } = req.query;
+  const idValidate = { _id: userId };
+  User.findOne(idValidate).then((user) => {
+    if (!user) return res.send("userId not exist");
+    const log = user.log.map((ex) => ({
+      ...ex.toObject(),
+      date: ex.date.toDateString(),
+    }));
+    const response = {
+      userId,
+      username: user.username,
+      count: user.log.length,
+      log,
+    };
+    res.json(response);
+  });
 });
 
 // Not found middleware
